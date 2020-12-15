d61b5c370e51defdfd0b63370e24a6dd99af9abf
Dataq Instruments <45690253+dataq-instruments@users.noreply.github.com>
2020-12-15 10:21:45 -0500

    Add files via upload

---
+++ CDCRawSample/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace CDCRawSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comport = new System.Windows.Forms.TextBox();
+            this.run = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.srate = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ps = new System.Windows.Forms.TextBox();
+            this.axXChart1 = new AxXCHARTLib.AxXChart();
+            ((System.ComponentModel.ISupportInitialize)(this.axXChart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Device COM port: ";
+            // 
+            // comport
+            // 
+            this.comport.Location = new System.Drawing.Point(111, 8);
+            this.comport.Name = "comport";
+            this.comport.Size = new System.Drawing.Size(28, 20);
+            this.comport.TabIndex = 1;
+            this.comport.Text = "11";
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(682, 9);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(58, 19);
+            this.run.TabIndex = 2;
+            this.run.Text = "Run";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(10, 428);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(39, 13);
+            this.Status.TabIndex = 3;
+            this.Status.Text = "Output";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "srate:";
+            // 
+            // srate
+            // 
+            this.srate.Location = new System.Drawing.Point(233, 8);
+            this.srate.Name = "srate";
+            this.srate.Size = new System.Drawing.Size(36, 20);
+            this.srate.TabIndex = 6;
+            this.srate.Text = "1000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(295, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ps";
+            // 
+            // ps
+            // 
+            this.ps.Location = new System.Drawing.Point(319, 8);
+            this.ps.Name = "ps";
+            this.ps.Size = new System.Drawing.Size(32, 20);
+            this.ps.TabIndex = 8;
+            this.ps.Text = "7";
+            // 
+            // axXChart1
+            // 
+            this.axXChart1.Enabled = true;
+            this.axXChart1.Location = new System.Drawing.Point(17, 44);
+            this.axXChart1.Name = "axXChart1";
+            this.axXChart1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axXChart1.OcxState")));
+            this.axXChart1.Size = new System.Drawing.Size(723, 371);
+            this.axXChart1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 450);
+            this.Controls.Add(this.axXChart1);
+            this.Controls.Add(this.ps);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.srate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.comport);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Protocol Demo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.axXChart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox comport;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox srate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ps;
+        private AxXCHARTLib.AxXChart axXChart1;
+    }
+}
+
---
+++ CDCRawSample/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CDCRawSample
+{
+    public partial class Form1 : Form
+    {
+        int portname = 11;
+        static SerialPort serialPort;
+        static byte [] ADCBuffer;
+        static int headADCBuffer=0;
+        static int tailADCBuffer=0;
+        Thread readThread;
+        static int AvailableBytes=0;
+        static int maxAvailableBytes = 0;
+        static string mysrate;
+        static string myps;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ADCBuffer = new byte [200000];
+            comport.Text = portname.ToString();
+
+            readThread = new Thread(Read);
+            readThread.Priority = ThreadPriority.Highest;
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            mysrate = srate.Text;
+            myps = ps.Text;
+
+            serialPort = new SerialPort();
+            serialPort.PortName = "COM"+comport.Text;
+            serialPort.ReadTimeout = 10;
+            serialPort.WriteTimeout = 50;
+            serialPort.ReadBufferSize = 4096 * 1024;  //big buffer
+
+            try
+            {
+                serialPort.Open();
+                serialPort.DiscardInBuffer();
+            }
+            catch
+            {
+                Status.Text = "Failed to open the port";
+                return;
+            }
+
+            timer1.Enabled = true;
+            if (readThread.IsAlive==false)
+            readThread.Start();
+
+            Setup();
+
+        }
+
+        private static void Read()
+        {
+            int i;
+            while (true)
+            {
+                try
+                {
+                    AvailableBytes = serialPort.BytesToRead;
+                    if (AvailableBytes > maxAvailableBytes) maxAvailableBytes = AvailableBytes;
+                     AvailableBytes = AvailableBytes- (AvailableBytes % 2);
+                }
+                catch
+                {
+                    AvailableBytes = 0;
+                }
+
+                if (AvailableBytes > 0)
+                {
+                    if (AvailableBytes > (ADCBuffer.Length - headADCBuffer)) AvailableBytes = ADCBuffer.Length - headADCBuffer;
+                    i=serialPort.Read(ADCBuffer, headADCBuffer, AvailableBytes);
+                    headADCBuffer = headADCBuffer + i;
+                    if (headADCBuffer >= ADCBuffer.Length) headADCBuffer = 0;
+                }
+            }
+        }
+
+        static void WriteRes(string s)
+        {
+            try
+            {
+                serialPort.WriteLine(s);
+            }
+            catch (TimeoutException) { }
+        }
+
+        public static void Setup()
+        {
+            WriteRes("stop\r");
+            Thread.Sleep(100);
+            serialPort.DiscardInBuffer();
+
+            WriteRes("encode 0\r");
+
+            WriteRes("slist 0 0\r");
+            WriteRes("srate "+mysrate+ "\r");
+            WriteRes("ps "+ myps+"\r");
+
+            WriteRes("dec 1\r");
+            WriteRes("deca 1\r");
+
+            Thread.Sleep(500); //So that we don't need to separate echo with real data
+            serialPort.DiscardInBuffer();
+
+            WriteRes("start\r");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int availablebyte = headADCBuffer-tailADCBuffer;
+            if (availablebyte < 0) availablebyte = availablebyte + ADCBuffer.Length;
+
+            int availablescan= availablebyte / 2;
+
+            if (availablescan>0)
+            {
+                int j;
+
+                Int16[,] PlotData = new Int16[1, availablescan];
+
+                Int16 n;
+                int takeindex;
+
+                for (j = 0, takeindex=tailADCBuffer; j < availablescan; j++)
+                {
+                    n = BitConverter.ToInt16(ADCBuffer, takeindex);
+                    takeindex = takeindex + 2;
+                    PlotData[0, j] = ADCBuffer[takeindex];
+                    if (takeindex >= ADCBuffer.Length) takeindex = 0;
+                }
+
+
+                axXChart1
+                    .Chart(PlotData);
+
+                Status.Text = PlotData[0,availablescan-1].ToString();
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                WriteRes("stop\r");
+                Thread.Sleep(200);
+                serialPort.Close();
+            }
+            catch { };
+        }
+    }
+}
